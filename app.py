63eee30e89b9ce7ea131388da3b3cd0fcb9f16c5
DistortedSignal <distortedsignal@gmail.com>
2016-04-05 09:11:38 -0700

    Adding a rough sketch of what the API for this should look like.

---
+++ app.py
@@ -0,0 +1,26 @@
+from Flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    # Return a static HTML page
+    pass
+
+@app.route("/<username>")
+def user_page(username):
+    # If this was real, I would want this to be a post request and run any
+    # password given against an excellent password hashing function with a
+    # generous work factor (naturally, I would include a large, random salt in 
+    # the database on a per-user basis)
+    pass
+
+@app.route("/<username>/with/<other_username>")
+def text_page(username, other_username):
+    pass
+
+@app.route("/<username>/with/<other_username>/new")
+def new_messages(username, other_username):
+    pass
+
+if __name__ == "__main__":
+    app.run()
