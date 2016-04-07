b6e93d28c54712f67b35a9f310ac9f1e713f061c
DistortedSignal <distortedsignal@gmail.com>
2016-04-06 21:17:45 -0700

    Removing pass because it would never be hit.

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 def main_page():
     # Return a static HTML page
     return front_page
-    pass
 
 @app.route("/<username>")
 def user_page(username):
-    # If this was real, I would want this to be a post request and run any
-    # password given against an excellent password hashing function with a
-    # generous work factor (naturally, I would include a large, random salt in 
-    # the database on a per-user basis)
+    # If this was real, I would want this to be architected in a radically 
+    # different fashion. Login should happen on the server, not as a get 
+    # request from the client. At a minimum, this should be a post request and 
+    # run any password given against an excellent password hashing function 
+    # with a generous work factor (naturally, I would include a large, random 
+    # salt in the database on a per-user basis)
     pass
 
 @app.route("/<username>/with/<other_username>")
